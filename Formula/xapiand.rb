df5a13fe8340c40a6a015e3a08d83ede37592499
German M. Bravo <german.mb@gmail.com>
2019-01-10 13:36:27 -0600

    Xapiand upgraded to v0.8.9

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -1,14 +1,14 @@
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
   homepage "https://kronuz.io/Xapiand"
-  url "https://github.com/Kronuz/Xapiand/archive/v0.8.8.tar.gz"
-  sha256 "a2c32d11e8aabacfa0450a5c2b4eeb048fa9a34bd382bb4ed6580798b6ffb571"
+  url "https://github.com/Kronuz/Xapiand/archive/v0.8.9.tar.gz"
+  sha256 "957e0b52fef1dfc9b37d50f8a7183cd07a19331f83d0332df58254d26fc0fa73"
   head "https://github.com/Kronuz/Xapiand.git"
 
   bottle do
     root_url "https://kronuz.io/homebrew-tap"
     cellar :any
-    sha256 "771d95ef157738957e8b0d59927579b7ed75a226ff4cf45cc38878f84759a53c" => :mojave
+    sha256 "de7b726b0eaae13d271058964c800486951ec9d047d3be90f910c2364965ca13" => :mojave
   end
 
   depends_on "cmake" => :build
@@ -17,10 +17,16 @@ class Xapiand < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DCCACHE_FOUND=CCACHE_FOUND-NOTFOUND", *std_cmake_args
+      system "cmake", "..", "-DCCACHE_FOUND=CCACHE_FOUND-NOTFOUND", "-DPREFIX=#{HOMEBREW_PREFIX}", *std_cmake_args
       system "make"
       system "make", "install"
     end
+  end
+
+  def post_install
+    (var/"run").mkpath
+    (var/"db/xapiand").mkpath
+    (var/"log").mkpath
   end
 
   test do
