8cbfd8a5f42753305eebb67e1e9e7eb2d559cabe
German M. Bravo <german.mb@gmail.com>
2019-02-20 12:17:46 -0600

    Xapiand updated

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -1,14 +1,14 @@
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
   homepage "https://kronuz.io/Xapiand"
-  url "https://github.com/Kronuz/Xapiand/archive/v0.10.0.tar.gz"
-  sha256 "837daaa933097177c21db428e4ebf88ee3f5b8da69240bf84ce1ea7ba62099bd"
+  url "https://github.com/Kronuz/Xapiand/archive/v0.10.1.tar.gz"
+  sha256 "22f86fa2cf9fe60019d0e7b89c22414650065241f51850cf66326ebe0a41bb1a"
   head "https://github.com/Kronuz/Xapiand.git"
 
   bottle do
     root_url "https://kronuz.io/homebrew-tap"
     cellar :any
-    sha256 "07be76b209105f6badcab06ca5a63b582f57c96b853b518bec17a19bcea241bf" => :mojave
+    sha256 "4c6230dccbcebcbbec6a582957af802306c3c59ec031423d3ad5e3f53c653281" => :mojave
   end
 
   depends_on "cmake" => :build
