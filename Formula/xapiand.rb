6ed8e2e54d7b0dab78d088ab89c7485efc17a103
Travis CI <travis@travis-ci.org>
2019-07-12 16:27:07 +0000

    Xapiand updated to v0.26.0 via Travis build 79

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -1,15 +1,15 @@
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
   homepage "https://kronuz.io/Xapiand"
-  url "https://github.com/Kronuz/Xapiand/archive/v0.25.0.tar.gz"
-  sha256 "d5ac1adb2f7c693b775981ddb1a754a0cac190ba6da4ff25491f5264fc6dcfd9"
+  url "https://github.com/Kronuz/Xapiand/archive/v0.26.0.tar.gz"
+  sha256 "87be53cae2262ae7eef3905dc56d0b560aa865280075d6987c7430054cb18349"
   head "https://github.com/Kronuz/Xapiand.git"
 
   bottle do
     root_url "https://kronuz.io/homebrew-tap"
     cellar :any
+    sha256 "3ae84ccbc31782ae74b8c0e732a4f4a22723684154ae98ea8d5222d201df2db3" => :high_sierra
     sha256 "913db89b5485bfcb5c9920502c5a408caeed6efd4ede770aa0ba885d6ae220eb" => :mojave
-    sha256 "71606e81f6b93531c82d7493966c39a63e70027cd98d914d6dd39f942a85f003" => :high_sierra
   end
 
   depends_on "icu4c"
